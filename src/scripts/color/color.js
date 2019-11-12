e34da2c75d36e8331db3a0a8a2ca475b8d530271
Daniel Benmore <dbenmore@users.noreply.github.com>
2019-11-12 14:18:37 +0000

    Update packages and use rgb package

--- src/scripts/color/color.js
+++ src/scripts/color/color.js
@@ -1,34 +1,10 @@
-// modified from https://stackoverflow.com/a/53936623
-
-const hexToRGBA = (hex) => {
-  const getChunksFromString = (st, chunkSize) => st.match(new RegExp(`.{${chunkSize}}`, 'g'))
-  const convertHexUnitTo256 = (hexStr) => parseInt(hexStr.repeat(2 / hexStr.length), 16)
-  const getAlphafloat = (a) => { return a / 256 }
-  const chunkSize = Math.floor((hex.length - 1) / 3)
-  const hexArr = getChunksFromString(hex.slice(1), chunkSize)
-  const [r, g, b, a] = hexArr.map(convertHexUnitTo256)
-
-  if (a !== undefined) {
-    return `rgba(${r}, ${g}, ${b}, ${getAlphafloat(a)})`
-  } else {
-    return `rgb(${r}, ${g}, ${b})`
-  }
-}
+var rgb = require("rgb")
 
 const configToRGBA = (colorArr) => {
   const convertedColors = []
   colorArr.forEach(color => {
-    if (color.startsWith('#')) {
-      convertedColors.push(hexToRGBA(color))
-    } else if (color.startsWith('rgb')) {
-      convertedColors.push(color)
-    } else if (color === 'transparent') {
-      convertedColors.push('rgba(0, 0, 0, 0)')
-    } else {
-      throw new Error(`${color} is invalid`)
-    }
+    convertedColors.push(rgb(color).replace(/\,/g, ', '))
   })
-
   return convertedColors
 }
 
@@ -41,7 +17,6 @@ const test = (computedColor, allowedColors, allowTransparent) => {
 }
 
 module.exports = {
-  hexToRGBA,
   test,
   configToRGBA
 }
--- src/scripts/color/color.spec.js
+++ src/scripts/color/color.spec.js
@@ -1,27 +1,9 @@
 const color = require('./color.js')
 
-describe('hexToRGBA', () => {
-  test('3 digit hex is converted into rgb', () => {
-    expect(color.hexToRGBA('#000')).toBe('rgb(0, 0, 0)')
-  })
-
-  test('6 digit hex is converted into rgb', () => {
-    expect(color.hexToRGBA('#000000')).toBe('rgb(0, 0, 0)')
-  })
-
-  test('4 digit hex is converted into rgba', () => {
-    expect(color.hexToRGBA('#000a')).toBe('rgba(0, 0, 0, 0.6640625)')
-  })
-
-  test('8 digit hex is converted into rgb', () => {
-    expect(color.hexToRGBA('#000000aa')).toBe('rgba(0, 0, 0, 0.6640625)')
-  })
-})
-
 describe('color configToRGBA', () => {
   test('passing colour test', () => {
-    const colorsToConvert = ['rgba(0, 0, 0, 123)', 'rgb(0, 0, 0)', '#FFF', '#1234', '#FFFFFF']
-    expect(color.configToRGBA(colorsToConvert)).toStrictEqual(['rgba(0, 0, 0, 123)', 'rgb(0, 0, 0)', 'rgb(255, 255, 255)', 'rgba(17, 34, 51, 0.265625)', 'rgb(255, 255, 255)'])
+    const colorsToConvert = ['rgba(0, 0, 0, 0.123)', 'rgb(0,0,0)', '#FFF', '#1234', '#FFFFFF']
+    expect(color.configToRGBA(colorsToConvert)).toStrictEqual(['rgba(0, 0, 0, 0.123)', 'rgb(0, 0, 0)', 'rgb(255, 255, 255)', 'rgba(17, 34, 51, 0.26666666666666666)', 'rgb(255, 255, 255)'])
   })
 })
 
